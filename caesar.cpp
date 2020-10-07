f3806f380281c5c8e17845ce19c3b43306a3fec3
bh9232 <bhong9232@gmail.com>
2020-10-07 16:56:02 -0400

    6

--- caesar.cpp
+++ caesar.cpp
@@ -10,31 +10,31 @@ caesar.cpp contains code for functions from caesar.h
 #include <string>
 #include "caesar.h"
 
-char shiftChar(char c, int rshift){ //helper method
+char shiftChar(char c, int shift){ //helper method
 
   if(((int) c >= 65 && c <= 90) || ((int) c >= 97 && c <= 122)){//if alphabet ASCII
 
     //if UpperCase letters
     if((int) c >= 65 && (int)c <= 90){
-      c = ((int)c + rshift - 65) % 26 + 65; 
+      c = ((int)c + shift - 65) % 26 + 65; 
     } 
 
     //if LowerCase letters
     else if ((int) c >= 97 && (int)c <= 122){
-      c = ((int)c + rshift - 97) % 26 + 97;  
+      c = ((int)c + shift - 97) % 26 + 97;  
     }
   }
   
   return c;
 }
 
-std::string encryptCaesar(std::string plaintext, int rshift){
+std::string encryptCaesar(std::string plaintext, int shift){
 
   std::string ciphertext;
   int size = plaintext.length();
   
   for(int i = 0; i < size; i++){
-    ciphertext += shiftChar(plaintext[i], rshift);
+    ciphertext += shiftChar(plaintext[i], shift);
   }
   return ciphertext;
 }
